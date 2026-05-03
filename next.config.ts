87f85d6a29a30666c58c5f7784ffc968364955c8
Sertug17 <104278804+Sertug17@users.noreply.github.com>
2026-05-03 10:11:53 +0300

    fix: upgrade to Next.js 16 with Turbopack

--- next.config.ts
+++ next.config.ts
@@ -1,24 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  productionBrowserSourceMaps: false,
-  webpack: (config, { isServer }) => {
-    config.externals.push(
-      "pino-pretty",
-      "lokijs",
-      "encoding",
-      "@react-native-async-storage/async-storage"
-    );
-
-    // Disable terser webpack plugin (use SWC instead)
-    config.optimization.minimize = true;
-    config.optimization.minimizer = config.optimization.minimizer.filter(
-      (plugin: { constructor: { name: string } }) =>
-        plugin.constructor.name !== "TerserPlugin"
-    );
-
-    return config;
-  },
+  turbopack: {},
 };
 
 export default nextConfig;
