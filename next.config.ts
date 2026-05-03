4c3507f0acf0832aa2d4d2a69718207ff64fbce6
Sertug17 <104278804+Sertug17@users.noreply.github.com>
2026-05-03 09:49:25 +0300

    fix: use SWC minifier instead of Terser to avoid webpack error

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  productionBrowserSourceMaps: false,
+  webpack: (config, { isServer }) => {
     config.externals.push(
       "pino-pretty",
       "lokijs",
       "encoding",
       "@react-native-async-storage/async-storage"
     );
+
+    // Disable terser webpack plugin (use SWC instead)
+    config.optimization.minimize = true;
+    config.optimization.minimizer = config.optimization.minimizer.filter(
+      (plugin: { constructor: { name: string } }) =>
+        plugin.constructor.name !== "TerserPlugin"
+    );
+
     return config;
   },
 };
